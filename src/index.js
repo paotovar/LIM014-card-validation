f2828d7e9bb5cf738af9043a127743648a006335
paotovar <ptovargavilan@gmail.com>
2021-01-23 23:47:55 -0500

    Realizando la modificación del index.js creando función validar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import validator from './validator.js';
 
-console.log(validator);
+/*console.log(validator);*/
+  
+document.getElementById("pay").addEventListener("click", validarCard); 
+
+
+function validarCard(event){
+    document.querySelector(".secondContainer").style.display = "block";
+    document.querySelector(".firstContainer").style.display = "none";
+
+
+    event.preventDefault();
+    var userNumInput = document.getElementById("cardNumber").value;
+    let tarjetaValida = validator.isValid(userNumInput);
+    let enmascarado = validator.maskify(userNumInput); 
+    
+
+    if (tarjetaValida == true){
+    
+        document.getElementById("secondContainer").innerHTML = "La tarjeta " + enmascarado + " es válida";
+    }
+    else {
+    
+        document.getElementById("secondContainer").innerHTML = "La tarjeta " + enmascarado + " es inválida";
+
+    }
+
+}
+
